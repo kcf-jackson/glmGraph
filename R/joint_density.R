025ac49c0ac994a80a537b9ec2b5fbd638ba3af3
kcf-jackson <kcf.jackson@gmail.com>
2017-04-09 15:41:13 +1000

    Re-organise functions

--- R/joint_density.R
+++ R/joint_density.R
@@ -1,57 +1,8 @@
-#' Compute the likelihood for a single data-point given the complete
-#' factorisation table
-#' @keywords internal
-#' @param table0 dataframe; A factorisation table, output from "build_conditional".
-#' @param data0 matrix or dataframe, the data.
-#' @param log TRUE / FALSE; returns loglikelihood / likelihood.
-#' @export
-compute_likelihood <- function(table0, data0, log = TRUE) {
-  nr <- nrow(data0)
-  seq(nr) %>%
-    purrr::map_dbl(
-      ~compute_datum_likelihood(table0, data0[nr, ], log = log)
-    ) %>%
-    sum()
-}
-
-
-#' Compute the likelihood for a single data-point given the complete
-#' factorisation table
-#' @keywords internal
-#' @param table0 dataframe; A factorisation table, output from "build_conditional".
-#' @param x data vector; one data point of multiple features/covariates.
-#' @param log TRUE / FALSE; returns loglikelihood / likelihood.
-compute_datum_likelihood <- function(table0, x, log = TRUE) {
-  table0 %<>% update_conditional_mean(x)
-
-  num_components <- nrow(table0)
-  loglikelihood <- 0
-  for (i in rev(seq(num_components))) {
-    current <- table0[i, ]
-
-    like_FUN <- current %>%
-      magrittr::use_series("likelihood") %>%
-      magrittr::extract2(1)
-
-    parameters <- current %>%
-      magrittr::use_series("parameters") %>%
-      magrittr::extract2(1)
-    # update parameters based on the conditional mean
-    parameters %<>%
-      mean2parameters(mu = table0$mean, family = table0$family) %>%
-      append(list(x = x[i], log = log))
-
-    loglikelihood <- loglikelihood + do.call(like_FUN, parameters)
-  }
-  loglikelihood
-}
-
-
 #' Specify the distribution for each conditional density
 #' @param df0 A factorisation table; output from "factorise".
 #' @param family Characters string; the exponential family of distribution.
 #' It must be one of c("gaussian", "gamma", "poisson", "binomial", "multinomial",
-#' "quadibinomial", "quasipoisson")
+#' "quasibinomial", "quasipoisson")
 #' @export
 build_conditional <- function(df0, family) {
   df0 %>%
---
+++ R/likelihood.R
@@ -0,0 +1,47 @@
+#' Compute the likelihood for a single data-point given the complete
+#' factorisation table
+#' @keywords internal
+#' @param table0 dataframe; A factorisation table, output from "build_conditional".
+#' @param data0 matrix or dataframe, the data.
+#' @param log TRUE / FALSE; returns loglikelihood / likelihood.
+#' @export
+compute_likelihood <- function(table0, data0, log = TRUE) {
+  nr <- nrow(data0)
+  seq(nr) %>%
+    purrr::map_dbl(
+      ~compute_datum_likelihood(table0, data0[nr, ], log = log)
+    ) %>%
+    sum()
+}
+
+
+#' Compute the likelihood for a single data-point given the complete
+#' factorisation table
+#' @keywords internal
+#' @param table0 dataframe; A factorisation table, output from "build_conditional".
+#' @param x data vector; one data point of multiple features/covariates.
+#' @param log TRUE / FALSE; returns loglikelihood / likelihood.
+compute_datum_likelihood <- function(table0, x, log = TRUE) {
+  table0 %<>% update_conditional_mean(x)
+
+  num_components <- nrow(table0)
+  loglikelihood <- 0
+  for (i in rev(seq(num_components))) {
+    current <- table0[i, ]
+
+    like_FUN <- current %>%
+      magrittr::use_series("likelihood") %>%
+      magrittr::extract2(1)
+
+    parameters <- current %>%
+      magrittr::use_series("parameters") %>%
+      magrittr::extract2(1)
+    # update parameters based on the conditional mean
+    parameters %<>%
+      mean2parameters(mu = table0$mean, family = table0$family) %>%
+      append(list(x = x[i], log = log))
+
+    loglikelihood <- loglikelihood + do.call(like_FUN, parameters)
+  }
+  loglikelihood
+}
