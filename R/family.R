7e6e1a5f81f0f1d693cfe5f911bf14085e5b6084
kcf-jackson <kcf.jackson@gmail.com>
2017-04-09 14:16:14 +1000

    Add mean2parameters; remove multinomial support temporarily

--- R/family.R
+++ R/family.R
@@ -1,3 +1,49 @@
+# USE FAMILY BY FAMILY PARTITION
+# family_reference_table <- function() {
+#   data.frame(
+#     family = c("gaussian", "gamma", "poisson", "binomial", "multinomial",
+#       "quadibinomial", "quasipoisson"),
+#     like_FUN = list(
+#       "gaussian" = dnorm,
+#       "gamma" = dgamma,
+#       "poisson" = dpois,
+#       "binomial" = dbinom,
+#       "multinomial" = dmultinom,
+#       "quasibinomial" = VGAM::dbetabinom.ab,
+#       "quasipoisson" = dnbinom
+#     ),
+#     sim_FUN = list(
+#       "gaussian" = rnorm,
+#       "gamma" = rgamma,
+#       "poisson" = rpois,
+#       "binomial" = rbinom,
+#       "multinomial" = rmultinom,
+#       "quasibinomial" = VGAM::rbetabinom.ab,
+#       "quasipoisson" = rnbinom
+#     ),
+#     invLink_FUN = list(
+#       "gaussian" = gaussian()$linkinv,
+#       "gamma" = Gamma()$linkinv,
+#       "poisson" = poisson()$linkinv,
+#       "binomial" = binomial()$linkinv,
+#       "multinomial" = multinomial()@linkinv,
+#       "quasibinomial" = VGAM::betabinomial()@linkinv,
+#       "quasipoisson" = quasipoisson()$linkinv
+#     ),
+#     parameters = list(
+#       list(mean = 0, sd = 1),
+#       list(shape = 1, rate = 1),
+#       list(lambda = 1),
+#       list(size = 1, prob = 0.5),
+#       list(size = 1, prob = rep(0.5, 5)),
+#       list(size = 1, shape1 = 1, shape2 = 1),
+#       list(size = 1, mu = 1)
+#     )
+#   )
+# }
+
+
+
 #' Return the density function of a family object
 #' @keywords internal
 family2likeFUN <- function(family, ...) {
@@ -7,7 +53,7 @@ family2likeFUN <- function(family, ...) {
     "gamma" = dgamma,
     "poisson" = dpois,
     "binomial" = dbinom,
-    "multinomial" = dmultinom,
+    # "multinomial" = dmultinom,
     "quasibinomial" = VGAM::dbetabinom.ab,
     "quasipoisson" = dnbinom
   )
@@ -23,7 +69,7 @@ family2simFUN <- function(family, ...) {
     "gamma" = rgamma,
     "poisson" = rpois,
     "binomial" = rbinom,
-    "multinomial" = rmultinom,
+    # "multinomial" = rmultinom,
     "quasibinomial" = VGAM::rbetabinom.ab,
     "quasipoisson" = rnbinom
   )
@@ -39,7 +85,7 @@ family2parameters <- function(family, ...) {
     "gamma" = list(shape = 1, rate = 1),
     "poisson" = list(lambda = 1),
     "binomial" = list(size = 1, prob = 0.5),
-    "multinomial" = list(size = 1, prob = rep(0.5, 5)),
+    # "multinomial" = list(size = 1, prob = rep(0.5, 5)),
     "quasibinomial" = list(size = 1, shape1 = 1, shape2 = 1),
     "quasipoisson" = list(size = 1, mu = 1)
   )
@@ -68,8 +114,25 @@ family2invLinkFUN <- function(family, ...) {
 #' @param parameters a named list
 #' @param family0 characters string; the exponential family.
 mean2parameters <- function(mean0, parameters, family0) {
-  parameters <- switch(
-
-  )
+  if (family0 == "gaussian") {
+    parameters$mean <- mean0
+  }
+  if (family0 == "gamma") {
+    parameters$shape <- mean0 * parameters$rate
+  }
+  if (family0 == "poisson") {
+    parameters$lambda <- mean0
+  }
+  if (family0 == "binomial") {
+    parameters$prob <- mean0
+  }
+  # if (family0 == "multinomial") {}
+  if (family0 == "quasibinomial") {
+    # mu = size * a / (a + b) => a = mu / (n - mu) * b
+    parameters$shape1 <- mean0 / (parameters$size - mean0) * parameters$shape2
+  }
+  if (family0 == "quasipoisson") {
+    parameters$mu <- mean0
+  }
   parameters
 }
