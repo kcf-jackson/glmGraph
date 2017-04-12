54d2084de55c5a6cfdadec2e2f3b670e157ad34a
kcf-jackson <kcf.jackson@gmail.com>
2017-04-12 17:24:21 +1000

    Optimise likelihood function

--- R/likelihood.R
+++ R/likelihood.R
@@ -1,50 +1,49 @@
-#' Compute the likelihood for a single data-point given the complete
-#' factorisation table
+#' Compute the likelihood for the data given the complete factorisation table
 #' @param table0 dataframe; A factorisation table, output from "build_conditional".
 #' @param data0 matrix or dataframe, the data.
 #' @param log TRUE / FALSE; returns loglikelihood / likelihood.
 #' @export
 compute_likelihood <- function(table0, data0, log = TRUE) {
   if (!(class(data0) %in% c("matrix", "data.frame"))) {
-    stop("You have only one datapoint and it is not of class 'matrix' or 'data.frame'.")
+    stop("Your data is not of class 'matrix' or 'data.frame'.")
   }
-  nr <- nrow(data0)
-  seq(nr) %>%
-    purrr::map_dbl(
-      ~compute_datum_likelihood(table0, data0[.x, ], log = log)
-    ) %>%
-    sum()
-}
-
-#' Compute the likelihood for a single data-point given the complete
-#' factorisation table
-#' @keywords internal
-#' @param table0 dataframe; A factorisation table, output from "build_conditional".
-#' @param x data vector; one data point of multiple features/covariates.
-#' @param log TRUE / FALSE; returns loglikelihood / likelihood.
-compute_datum_likelihood <- function(table0, x, log = TRUE) {
-  table0 %<>% update_conditional_mean(x)
-
   num_components <- nrow(table0)
   loglikelihood <- 0
-  for (i in seq(num_components)) {
-    current <- table0[i, ]
-
-    like_FUN <- current %>%
-      magrittr::use_series("likelihood_FUN") %>%
-      magrittr::extract2(1)
-
-    parameters <- current %>%
-      magrittr::use_series("parameters") %>%
-      magrittr::extract2(1)
-    # update parameters based on the conditional mean
-    parameters %<>%
-      mean2parameters(mu = current$mean, family = current$family) %>%
-      append(list(x = x[i], log = log))
-
-    loglikelihood <- loglikelihood + do.call(like_FUN, parameters)
+  complete_index <- c()
+  while (!setequal(complete_index, seq(num_components))) {
+    current_index <- table0 %>% get_available_rows(exclude = complete_index)
+    for (i in current_index) {
+      current <- table0[i,]
+      resp <- current$fixed[[1]]
+      given <- current$given[[1]]
+      loglikelihood <- loglikelihood +
+        eval_marginal_likelihood(current, data0[,resp], data0[,given])
+      print(i)
+      print(current$beta[[1]])
+      print(eval_marginal_likelihood(current, data0[,resp], data0[,given]))
+    }
+    complete_index <- c(complete_index, current_index)
   }
   loglikelihood
+}
+
+#' @keywords internal
+eval_marginal_likelihood <- function(marginal, resp_variable, covariates) {
+  #update conditional mean
+  invLink <- marginal$invLink_FUN[[1]]
+  beta <- marginal$beta[[1]]
+  mu <- get_batch_eta(beta, cbind(1, covariates)) %>% invLink()
+  #update parameters and compute the likelihood
+  parameters <- marginal$parameters[[1]]
+  family <- marginal$family[[1]]
+  like_FUN <- marginal$likelihood_FUN[[1]]
+  purrr::map2_dbl(
+    .x = resp_variable, .y = mu,
+    .f = ~do.call(
+      like_FUN,
+      append(mean2parameters(parameters, .y, family), list(x = .x, log = TRUE))
+    )
+  ) %>% sum()
 }
 
 #' Update the conditional mean given the data
@@ -68,3 +67,9 @@ compute_mean <- function(pos, x, beta, inv_link, default) {
   if (length(x[pos]) == 0) { return(beta) }
   inv_link(sum(c(1, x[pos]) * beta, na.rm = TRUE)) #intercept
 }
+
+#' @keywords internal
+get_model_likelihood <- function(table0) {
+  attr(table0, "loglikelihood")
+}
+
