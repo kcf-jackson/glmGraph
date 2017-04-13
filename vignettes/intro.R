b0c71d907775f660bf55ead9527f18fde92d8274
kcf-jackson <kcf.jackson@gmail.com>
2017-04-14 02:12:38 +1000

    Update vignettes

--- vignettes/intro.R
+++ vignettes/intro.R
@@ -1,30 +1,40 @@
-## ---- eval=F-------------------------------------------------------------
-#  library(magrittr)
-#  library(glmGraph)
-#  
-#  # Create graph
-#  rgraph <- create_random_graph(10, p = 0.2)
-#  plot_graph(rgraph, vertex.size = 30)
-#  
-#  # Factorise the joint density into products of conditionals
-#  table0 <- factorise(rgraph)
-#  
-#  # Specify the distribution of the conditionals
-#  m <- 7
-#  family <- c(rep("gaussian", m), rep("gamma", nrow(table0) - m))
-#  table0 %<>% build_conditional(family = family)
-#  
-#  # Simulate data
-#  data0 <- simulate_data(table0, n = 100)
+## ---- eval=T, results='asis', fig.height=4, fig.width=4------------------
+library(magrittr)
+library(glmGraph)
+
+# Create a random graph
+num_nodes <- 10
+graph_connectedness <- 0.2  #probability of having an edge between two nodes
+rgraph <- create_random_graph(num_nodes, p = graph_connectedness)
+knitr::kable(rgraph)
+
+# Plot graph
+plot_graph(rgraph, vertex.size = 30)  #vertex.size is the plot-size of the nodes
+
+## ---- eval=T, results='asis', fig.height=4, fig.width=4------------------
+# Specify the dependence with the GLM family 
+# 1. factorise the graph
+table0 <- factorise(rgraph)
+knitr::kable(table0)
+# 2. specify a GLM family for each component
+family <- c(rep("gaussian", 7), rep("gamma", 3))
+table0 <- build_conditional(table0, family = family)
+
+## ---- eval=T, results='asis', fig.height=4, fig.width=4------------------
+# Simulate data
+data0 <- simulate_data(table0, n = 2000)
+
+## ---- eval=T, results='asis', fig.height=4, fig.width=4------------------
+# Create a new table to fit the data
+table1 <- rgraph %>% factorise() %>% build_conditional(family)
+table1 %<>% MLE_graph(data0)
+
+# Compare the truth and the fitted model
+knitr::kable(data.frame(cbind(
+    true_beta = table0$beta %>% purrr::map(~round(.x, 3)), 
+    fitted_beta = table1$beta %>% purrr::map(~round(.x, 3))
+)))
 
 ## ---- eval = F-----------------------------------------------------------
-#  # Create a new table to fit data
-#  table1 <- rgraph %>% factorise() %>% build_conditional(family)
-#  table1 %<>% MLE_graph(data0)
-#  
-#  # Compare the truth and the fitted model
-#  data.frame(cbind(true_beta = table0$beta, fitted_beta = table1$beta))
-#  compute_likelihood(table0, data0)
-#  get_model_likelihood(table1)
-#  compute_likelihood(table1, data0)
+#  s <- select_graph(data0, lambda = 1 / sqrt(nrow(data0)))
 
