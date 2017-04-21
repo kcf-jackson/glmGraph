0c7d34efb39c9bbe95eccc41a146ef359d7fc141
Felix Leung <felix.leungsc@gmail.com>
2017-04-21 18:07:45 +1000

    Add references for graph-based two-sample tests

--- R/model_evaluation.R
+++ R/model_evaluation.R
@@ -1,4 +1,4 @@
-#' Edge-count tests
+#' Graph-based two-sample tests
 #'
 #' Test if `df1` and `df2` come from the same distribution. This is a wrapper
 #' of `g.tests` in which the edge matrix of the similarity graph is based on a
@@ -16,6 +16,16 @@
 #' g_tests(data.frame(x = rnorm(100)), data.frame(x = runif(100)))
 #'
 #' @export
+#' @references Friedman J. and Rafsky L. Multivariate generalizations of the
+#' WaldWolfowitz and Smirnov two-sample tests. The Annals of Statistics,
+#' 7(4):697-717, 1979.
+#'
+#' Chen, H. and Friedman, J. H. A new graph-based two-sample test for
+#' multivariate and object data. Journal of the American Statistical
+#' Association, 2016.
+#'
+#' Chen, H., Chen, X. and Su, Y. A weighted edge-count two sample test for
+#' multivariate and object data. arXiv:1604.06515.
 g_tests <- function(df1, df2, method = "euclidean") {
   df <- dplyr::bind_rows(df1, df2)
   dis <- stats::dist(df, method = method)
