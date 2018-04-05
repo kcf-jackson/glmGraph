9c1eac2d8fda303529e841e3520041e02d9c8edc
kcf-jackson <kcf.jackson@gmail.com>
2018-04-06 03:21:02 +1000

    Add examples and progress bar switch

--- R/inference.R
+++ R/inference.R
@@ -1,13 +1,30 @@
 #' Fitting the GLM graphical model with MLE.
 #' @param table0 dataframe; the complete factorisation table, output from "build_conditional".
 #' @param data0 dataframe; the data.
-# #' @param engine function; the function to fit a GLM.
+#' @param progress T or F; whether to show progress bar.
 #' @return dataframe; the complete factorisation table with fitted beta.
+#' @examples
+#' \dontrun{
+#' library(magrittr)
+#'
+#' # Create a data generating process randomly and simulate data from it
+#' g_struct <- random_DAG(10) %>% factorise()
+#' truth <- build_conditional(g_struct, rep("gaussian", 10))
+#' sim_data <- simulate_data(truth, n = 1000)
+#'
+#' # Create a empty model with the given graph structure but with random coefficients
+#' g_model <- build_conditional(g_struct, rep("gaussian", 10))
+#' is_fitted(g_model)
+#' data.frame(rbind(truth = truth$beta, model = g_model$beta)) # compare coefficients before fitting
+#'
+#' g_model <- MLE_graph(g_model, sim_data, T)
+#' data.frame(rbind(truth = truth$beta, model = g_model$beta)) # compare coefficients after fitting
+#' }
 #' @export
-MLE_graph <- function(table0, data0) {
+MLE_graph <- function(table0, data0, progress = T) {
   nr <- nrow(table0)
   loglikelihood <- 0
-  # pb <- txtProgressBar(1, nr, style=3)
+  if (progress) pb <- txtProgressBar(1, nr, style=3)
   for (i in seq(nr)) {
     current <- table0[i, ]
     c_family <- current$family[[1]]
@@ -25,7 +42,7 @@ MLE_graph <- function(table0, data0) {
     current$parameters[[1]] %<>%
       dispersion2parameters(glm_model$dispersion, c_family)
     table0[i, ] <- current
-    # setTxtProgressBar(pb, i)
+    if (progress) setTxtProgressBar(pb, i)
   }
   attr(table0, "loglikelihood") <- loglikelihood
   attr(table0, "beta") <- "fitted"
